43ee936c4ed4d3198a9e6b9677f8a53f49eac0d2
Miguel Flores-Acton <mfacton1@gmail.com>
2024-09-04 13:38:03 -0400

    Implement linked files

---
+++ templates/link.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ./link.bash {folder}
+
+# Check if the number of arguments is not equal to 1
+if [ "$#" -ne 1 ]; then
+    echo "Error: You must provide exactly 1 argument"
+    exit 1
+fi
+
+# Get clean folder name
+name=${1%/}
+# Create folder if not exists
+if [ ! -d "$name" ]; then
+    mkdir $name
+fi
+
+link() {
+    rm -f "$2"
+    ln -s "$1" "$2"
+    echo "Symlink created for $2"
+}
+
+files=(".gitignore" "sym-lib-table" "fp-lib-table" "fabrication-toolkit-options.json")
+for file in "${files[@]}"; do
+    link "base/$file" "$name/$file"
+done
+
+files=("kicad_pro" "kicad_sch" "kicad_prl")
+for file in "${files[@]}"; do
+    link "base/$file" "$name/$name.$file"
+done
+
